1fe836bd31ec7110814be11b913f7124e6c23066
Phil Hagelberg <technomancy@gmail.com>
2012-08-31 13:59:09 -0700

    Use Leiningen 2.

--- project.clj
+++ project.clj
@@ -8,4 +8,5 @@
                  [clj-json "0.3.2"]
                  [ring/ring-core "0.3.11"]
                  [ring/ring-jetty-adapter "0.3.11"]
-                 [org.jsoup/jsoup "1.6.1"]])
+                 [org.jsoup/jsoup "1.6.1"]]
+  :min-lein-version "2.0.0")
